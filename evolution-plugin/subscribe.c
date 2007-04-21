22a790df20c0ed88cc4726c0aae2add4abfa3142
Ondrej Jirman <ondrej.jirman@zonio.net>
2007-04-21 12:17:08 +0200

    implement autocompletion in subscribe

--- evolution-plugin/subscribe.c
+++ evolution-plugin/subscribe.c
@@ -18,9 +18,44 @@ struct subscribe_context
   GtkWindow* win;
   GtkTreeStore *model;
   GtkTreeView *tview;
+  EeeAccount* acc;
 };
 
-static gboolean load_calendars(EeeAccount* acc, char* prefix, GtkTreeStore* model)
+static gboolean load_users(EeeAccount* acc, char* prefix, GtkListStore* model)
+{
+  xr_client_conn* conn;
+  GError* err = NULL;
+  GSList *users, *iter;
+  GtkTreeIter titer_user;
+
+  conn = eee_server_connect_to_account(acc);
+  if (conn == NULL)
+    return FALSE;
+
+  users = ESClient_getUsers(conn, prefix ? prefix : "", &err);
+  if (err)
+  {
+    g_debug("** EEE ** Failed to get users list for user '%s'. (%d:%s)", acc->email, err->code, err->message);
+    xr_client_free(conn);
+    g_clear_error(&err);
+    return FALSE;
+  }
+
+  for (iter = users; iter; iter = iter->next)
+  {
+    char* user = iter->data;
+    gtk_list_store_append(model, &titer_user);
+    gtk_list_store_set(model, &titer_user, 0, user, -1);
+  }
+
+  g_slist_foreach(users, (GFunc)g_free, NULL);
+  g_slist_free(users);
+
+  xr_client_free(conn);
+  return TRUE;
+}
+
+static gboolean load_calendars(EeeAccount* acc, char* prefix, GtkTreeStore* model, struct subscribe_context* ctx)
 {
   xr_client_conn* conn;
   GError* err = NULL;
@@ -28,6 +63,8 @@ static gboolean load_calendars(EeeAccount* acc, char* prefix, GtkTreeStore* mode
   GtkTreeIter titer_user;
   GtkTreeIter titer_cal;
   int rs;
+
+  gtk_tree_store_clear(model);
 
   conn = eee_server_connect_to_account(acc);
   if (conn == NULL)
@@ -75,8 +112,28 @@ static gboolean load_calendars(EeeAccount* acc, char* prefix, GtkTreeStore* mode
   g_slist_foreach(users, (GFunc)g_free, NULL);
   g_slist_free(users);
 
+  gtk_tree_view_expand_all(ctx->tview);
+
   xr_client_free(conn);
   return 0;
+}
+
+static gboolean user_selected(GtkEntryCompletion *widget, GtkTreeModel *model, GtkTreeIter *iter, struct subscribe_context* ctx)
+{
+  char* user = NULL;
+  gtk_tree_model_get(model, iter, 0, &user, -1);
+  if (user)
+  {
+    load_calendars(ctx->acc, user, ctx->model, ctx);
+  }
+  return FALSE;
+}
+
+static gboolean user_insert_prefix(GtkEntryCompletion *widget, char* prefix, struct subscribe_context* ctx)
+{
+  if (prefix)
+    load_calendars(ctx->acc, prefix, ctx->model, ctx);
+  return FALSE;
 }
 
 static void on_subs_button_subscribe_clicked(GtkButton* button, struct subscribe_context* ctx)
@@ -108,6 +165,7 @@ void subscribe_gui_create(EeeAccount* acc)
   GtkTreeViewColumn *column;
 
   struct subscribe_context* c = g_new0(struct subscribe_context, 1);
+  c->acc = acc;
   c->xml = glade_xml_new(PLUGINDIR "/org-gnome-evolution-eee.glade", "subs_window", NULL);
 
   c->win = GTK_WINDOW(gtk_widget_ref(glade_xml_get_widget(c->xml, "subs_window")));
@@ -130,7 +188,20 @@ void subscribe_gui_create(EeeAccount* acc)
   //column = gtk_tree_view_get_column(c->tview, col_offset - 1);
   //gtk_tree_view_column_set_clickable(column, TRUE);
 
-  load_calendars(acc, "", c->model);
+  // setup autocompletion
+  GtkListStore* users_store = gtk_list_store_new(1, G_TYPE_STRING);
+  load_users(acc, "", users_store);
+  GtkEntryCompletion *completion;
+  completion = gtk_entry_completion_new();
+  gtk_entry_set_completion(GTK_ENTRY(glade_xml_get_widget(c->xml, "entry_email")), completion);
+  g_object_unref(completion);
+  gtk_entry_completion_set_model(completion, GTK_TREE_MODEL(users_store));
+  g_object_unref(users_store);
+  gtk_entry_completion_set_text_column(completion, 0);
+  gtk_entry_completion_set_inline_completion(completion, TRUE);
+  gtk_entry_completion_set_popup_single_match(completion, FALSE);
+  g_signal_connect(completion, "match-selected", G_CALLBACK(user_selected), c);
+  g_signal_connect(completion, "insert-prefix", G_CALLBACK(user_insert_prefix), c);
 
   SIGNAL_CONNECT(on_subs_button_cancel_clicked);
   SIGNAL_CONNECT(on_subs_button_subscribe_clicked);
