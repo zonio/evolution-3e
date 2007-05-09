94c17bf159338235281e0f05032f77c7b782de4b
Ondrej Jirman <ondrej.jirman@zonio.net>
2007-05-09 19:16:49 +0200

    less verbose debug

--- evolution-plugin/eee-calendar-config.c
+++ evolution-plugin/eee-calendar-config.c
@@ -20,7 +20,7 @@ static EeeAccountsManager* _mgr = NULL;
 int e_plugin_lib_enable(EPluginLib* ep, int enable)
 {
   if (getenv("EEE_EVO_DEBUG"))
-    xr_debug_enabled = XR_DEBUG_CALL|XR_DEBUG_HTTP;
+    xr_debug_enabled = XR_DEBUG_CALL;
   return 0;
 }
 
