b5dfb246fe8c207656d4172bd0448d165110eddd
Ondrej Jirman <ondrej.jirman@zonio.net>
2008-01-23 02:42:03 +0100

    source code GPLized

--- eds-plugin/e-cal-backend-3e.c
+++ eds-plugin/e-cal-backend-3e.c
@@ -1,12 +1,24 @@
-/***************************************************************************
- *  3E plugin for Evolution Data Server                                    *
- *                                                                         *
- *  Copyright (C) 2007 by Zonio                                            *
- *  www.zonio.net                                                          *
- *  Stanislav Slusny <stanislav.slusny@zonio.net>                          *
- *  Ondrej Jirman <ondrej.jirman@zonio.net>                                *
- *                                                                         *
- ***************************************************************************/
+/* 
+ * Authors: Ondrej Jirman <ondrej.jirman@zonio.net>
+ *          Stanislav Slusny <stanislav.slusny@zonio.net>
+ *
+ * Copyright 2007-2008 Zonio, s.r.o.
+ * 
+ * This file is part of evolution-3e.
+ *
+ * Libxr is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Libxr is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with evolution-3e.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "e-cal-backend-3e-priv.h"
 #include <libedataserver/e-xml-hash-utils.h>
