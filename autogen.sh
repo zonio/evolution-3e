55b9c3e128e4f20b248f899351465fb054fdfd90
Jan Sebetovsky <jan.sebetovsky@zonio.net>
2009-07-13 22:21:02 +0200

    Change of CFLAGS.

--- autogen.sh
+++ autogen.sh
@@ -9,4 +9,4 @@ cd $srcdir
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
-$srcdir/configure --enable-maintainer-mode "$@"
+$srcdir/configure --enable-maintainer-mode "$@" CFLAGS="-g -O0 -Wall -Wextra -Wno-unused-parameter -Wno-unused-variable -DHANDLE_LIBICAL_MEMORY=1"
